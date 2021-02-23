5869bda0f500a503d29f524d42768087a1031122
George Laney <64995389+GLaney@users.noreply.github.com>
2021-02-23 13:14:28 -0500

    forum models change

--- seproject/app/models.py
+++ seproject/app/models.py
@@ -7,10 +7,8 @@ from django.forms import ModelForm
 
 class forum(models.Model):
     name=models.CharField(max_length=200,default="anonymous" )
-    #email=models.CharField(max_length=200,null=True)
     topic= models.CharField(max_length=300)
     description = models.CharField(max_length=1000,blank=True)
-    #link = models.CharField(max_length=100 ,null =True)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     
     def __str__(self):
